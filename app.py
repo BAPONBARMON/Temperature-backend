2f1ff717c960da9499a5d4354cfa32f89623c36d
BAPONBARMON <experiment.mail.125@gmail.com>
2025-06-25 23:18:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
+API_KEY = "46ef8d93c0414f0f85f123007252506"
+
 @app.route("/", methods=["GET"])
 def get_temperature():
     city = request.args.get("city")
-    # Dummy data for now
-    temp_data = {
-        "Lanka": "34°C",
-        "Delhi": "37°C",
-        "Kolkata": "33°C"
-    }
-    temperature = temp_data.get(city, "Not Found")
-    return jsonify({
-        "city": city,
-        "temperature": temperature
-    })
+    if not city:
+        return jsonify({"error": "City not provided"}), 400
+
+    # OpenWeatherMap API URL
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    try:
+        response = requests.get(weather_url)
+        data = response.json()
+
+        if response.status_code == 200:
+            temp = data["main"]["temp"]
+            return jsonify({
+                "city": city,
+                "temperature": f"{temp}°C"
+            })
+        else:
+            return jsonify({
+                "city": city,
+                "temperature": "City not found."
+            }), 404
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run()
